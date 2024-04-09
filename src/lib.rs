03aebc54c32b58c6dccd6e6dac1b07d433fa3e86
Anton Romanov <theli.ua@gmail.com>
2024-04-08 22:52:46 -0700

    Add stub factory

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,61 @@
-use windows::core::GUID;
-use windows::core::*;
-use windows::Win32::Foundation::{CLASS_E_CLASSNOTAVAILABLE, E_POINTER};
+#![allow(non_snake_case)]
+use windows::{core::*, Win32::Foundation::*, Win32::System::WinRT::*};
 // use windows::Media::SystemMediaTransportControls;
-use windows::Win32::System::Variant::VariantToDouble;
+
+#[implement(IActivationFactory, ISystemMediaTransportControlsInterop)]
+struct ActivationFactory;
+
+impl IActivationFactory_Impl for ActivationFactory {
+    fn ActivateInstance(&self) -> Result<IInspectable> {
+        dbg!("AAA");
+        Err(E_NOTIMPL.into())
+    }
+}
+
+impl ISystemMediaTransportControlsInterop_Impl for ActivationFactory {
+    // Required method
+    fn GetForWindow(
+        &self,
+        appwindow: HWND,
+        riid: *const GUID,
+        mediatransportcontrol: *mut *mut std::ffi::c_void,
+    ) -> Result<()> {
+        dbg!(appwindow);
+        dbg!(unsafe { *riid });
+        unimplemented!()
+    }
+}
 
 #[no_mangle]
 extern "system" fn DllGetActivationFactory(
     name: std::mem::ManuallyDrop<HSTRING>,
     result: *mut *mut std::ffi::c_void,
 ) -> HRESULT {
-    eprintln!("Hello World!!!! {:?}", *name);
+    eprintln!("Hello World! {:?}", *name);
+
     if result.is_null() {
         return E_POINTER;
     }
 
-    // let mut factory: Option<IActivationFactory> = None;
+    let mut factory: Option<IActivationFactory> = None;
 
-    // if *name == "Sample.JsonValidator" {
-    // eprintln!("Hello World!!!! ");
-    //     factory = Some(JsonValidatorFactory.into());
-    // }
+    if *name == "Windows.Media.SystemMediaTransportControls" {
+        factory = Some(ActivationFactory.into());
+    }
+    dbg!(&factory);
 
-    CLASS_E_CLASSNOTAVAILABLE
     // Dereferencing `result` is safe because we've validated that the pointer is not null and
     // transmuting `factory` is safe because `DllGetActivationFactory` is expected to return an
     // `IActivationFactory` pointer and that's what `factory` contains.
-    // unsafe {
-    //     if let Some(factory) = factory {
-    //         *result = std::mem::transmute(factory);
-    //         S_OK
-    //     } else {
-    //         *result = std::ptr::null_mut();
-    //         CLASS_E_CLASSNOTAVAILABLE
-    //     }
-    // }
+    unsafe {
+        if let Some(factory) = factory {
+            *result = std::mem::transmute(factory);
+            S_OK
+        } else {
+            *result = std::ptr::null_mut();
+            CLASS_E_CLASSNOTAVAILABLE
+        }
+    }
 }
 
 #[no_mangle]
