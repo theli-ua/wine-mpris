b79d01d3c1e63eb241b41e93913e01db3cf859c6
Anton Romanov <theli.ua@gmail.com>
2024-04-13 22:24:17 -0700

    update build.sh, no more winegcc, native dll

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 set -e
-cargo build --release --target i686-unknown-linux-gnu
-winegcc -shared -spec windows.media.mediacontrol.spec target/i686-unknown-linux-gnu/release/libmediacontrol.a -loleaut32 -lpropsys -lcombase -m32 -o windows.media.mediacontrol.so
+cargo build --release --target i686-pc-windows-gnu
+cargo build --release --target x86_64-pc-windows-gnu
